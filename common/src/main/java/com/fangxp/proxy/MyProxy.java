3b1546ed3c434c61ffbf43836caa13ff7eecddad
xianpanfang <xianpanfang@hengtiansoft.com>
2016-12-13 10:13:53 +0800

    Initial commit

---
+++ common/src/main/java/com/fangxp/proxy/MyProxy.java
@@ -0,0 +1,12 @@
+package com.fangxp.proxy;
+
+/**
+ * Created by Administrator on 2016/12/7.
+ */
+public class MyProxy {
+
+    public static Object createProxyInstance() {
+        return null;
+    }
+
+}
